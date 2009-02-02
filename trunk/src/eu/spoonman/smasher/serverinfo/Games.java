163af4320fb4da2b6609a4b6bd4daf0fa0728655
tomasz.kalkosinski <tomasz.kalkosinski@ebad3e58-8269-11dd-ad27-0f66e57d2035>
2009-02-02 18:19:54 +0000

    Games enum cleanup.

--- trunk/src/eu/spoonman/smasher/serverinfo/Games.java
+++ trunk/src/eu/spoonman/smasher/serverinfo/Games.java
@@ -1,32 +1,12 @@
 package eu.spoonman.smasher.serverinfo;
 
-import java.io.File;
-import java.util.*;
-
+/**
+ * Supported games enum.
+ * 
+ * @author Tomasz Kalkosiński
+ * 
+ */
 public enum Games {
 
     QUAKE3ARENA;
-
-    private final String path = ("src.eu.spoonman.smasher.serverinfo.").replace(".", File.separator);
-    private final String extension = ".properties";
-    private List<String> files;
-
-    Games() {
-        fillFiles(toString());
-    }
-
-    Games(String... files) {
-        fillFiles(files);
-    }
-
-    private void fillFiles(String... files) {
-        this.files = new ArrayList<String>();
-        for (String file : files) {
-            this.files.add(path + file.toLowerCase() + extension);
-        }
-    }
-
-    public List<String> getFiles() {
-        return files;
-    }
 }
