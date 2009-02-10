9439cc56e632593a6bb47aac91b7c4f4f69120a0
tomasz.kalkosinski <tomasz.kalkosinski@ebad3e58-8269-11dd-ad27-0f66e57d2035>
2009-02-10 10:22:17 +0000

    Add NumbersPlayerInfoParserTest.

---
+++ trunk/tests/eu/spoonman/smasher/serverinfo/parser/playerinfo/NumbersPlayerInfoParserTest.java
@@ -0,0 +1,63 @@
+package eu.spoonman.smasher.serverinfo.parser.playerinfo;
+
+import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+import eu.spoonman.smasher.serverinfo.PlayerInfo;
+import eu.spoonman.smasher.serverinfo.ServerInfo;
+import eu.spoonman.smasher.serverinfo.TeamKey;
+import eu.spoonman.smasher.serverinfo.parser.ParserException;
+import eu.spoonman.smasher.serverinfo.parser.ServerInfoParser;
+
+public class NumbersPlayerInfoParserTest {
+    
+    private ServerInfo createServerInfo() {
+        ServerInfo serverInfo = new ServerInfo();
+        
+        serverInfo.getPlayerInfos().add(new PlayerInfo());
+        serverInfo.getPlayerInfos().add(new PlayerInfo());
+        serverInfo.getPlayerInfos().add(new PlayerInfo());
+        serverInfo.getPlayerInfos().add(new PlayerInfo());
+        serverInfo.getPlayerInfos().add(new PlayerInfo());
+        serverInfo.getPlayerInfos().add(new PlayerInfo());
+        
+        return serverInfo;
+    }
+
+    @Test
+    public void parseIntoServerInfoOne() throws ParserException {
+        ServerInfoParser parser = new NumbersPlayerInfoParser("testingAtt", 0, TeamKey.RED_TEAM, ";");
+        
+        ServerInfo serverInfo = createServerInfo();
+        serverInfo.getNamedAttributes().put("testingAtt", "0;1;4;5");
+        
+        parser.parseIntoServerInfo(serverInfo);
+        
+        assertEquals(TeamKey.RED_TEAM, serverInfo.getPlayerInfos().get(0).getTeamKey());
+        assertEquals(TeamKey.RED_TEAM, serverInfo.getPlayerInfos().get(1).getTeamKey());
+        assertEquals(null, serverInfo.getPlayerInfos().get(2).getTeamKey());
+        assertEquals(null, serverInfo.getPlayerInfos().get(3).getTeamKey());
+        assertEquals(TeamKey.RED_TEAM, serverInfo.getPlayerInfos().get(4).getTeamKey());
+        assertEquals(TeamKey.RED_TEAM, serverInfo.getPlayerInfos().get(5).getTeamKey());
+    }
+    
+    @Test
+    public void parseIntoServerInfoTwo() throws ParserException {
+        ServerInfoParser parser = new NumbersPlayerInfoParser("teams", -2, TeamKey.BLUE_TEAM, " ");
+        
+        ServerInfo serverInfo = createServerInfo();
+        serverInfo.getNamedAttributes().put("teams", " 2 4 7 ");
+        
+        parser.parseIntoServerInfo(serverInfo);
+        
+        assertEquals(TeamKey.BLUE_TEAM, serverInfo.getPlayerInfos().get(0).getTeamKey());
+        assertEquals(null, serverInfo.getPlayerInfos().get(1).getTeamKey());
+        assertEquals(TeamKey.BLUE_TEAM, serverInfo.getPlayerInfos().get(2).getTeamKey());
+        assertEquals(null, serverInfo.getPlayerInfos().get(3).getTeamKey());
+        assertEquals(null, serverInfo.getPlayerInfos().get(4).getTeamKey());
+        assertEquals(TeamKey.BLUE_TEAM, serverInfo.getPlayerInfos().get(5).getTeamKey());
+    }
+
+}
