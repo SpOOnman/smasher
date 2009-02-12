5537ceae70fe7c2f3c5834eafed5008b8711b3c6
tomasz.kalkosinski <tomasz.kalkosinski@ebad3e58-8269-11dd-ad27-0f66e57d2035>
2009-02-12 12:31:40 +0000

    Implement some TwoRowMatrix methods.

--- trunk/src/eu/spoonman/smasher/common/TwoRowMatrix.java
+++ trunk/src/eu/spoonman/smasher/common/TwoRowMatrix.java
@@ -24,23 +24,80 @@ import java.util.ArrayList;
  * Simple two row matrix with few convenience methods.
  * 
  * @author Tomasz Kalkosińśki
- *
+ * 
  */
 public class TwoRowMatrix {
-    
+
     private ArrayList<Integer> firstRow;
+
     private ArrayList<Integer> secondRow;
-    
+
+    public TwoRowMatrix() {
+        firstRow = new ArrayList<Integer>();
+        secondRow = new ArrayList<Integer>();
+    }
+
     /**
      * 
      */
-    public TwoRowMatrix(int size, int value) {
-        firstRow = new ArrayList<Integer> (size);
-        secondRow = new ArrayList<Integer> (size);
-        
-        for (int i = 0 ; i < size ; i++) {
+    public TwoRowMatrix(int size, Integer value) {
+        for (int i = 0; i < size; i++) {
             firstRow.add(value);
             secondRow.add(value);
         }
     }
+
+    /**
+     * Multiply this matrix by vector.
+     * 
+     * @param vector
+     * @return
+     */
+    public TwoRowMatrix multiply(ArrayList<Integer> vector) {
+        if (vector == null)
+            throw new IllegalArgumentException("vector is null");
+
+        if (vector.size() != firstRow.size() || vector.size() != secondRow.size())
+            throw new IllegalArgumentException("vector size is not equal to matrix dimension");
+
+        int firstSum = 0;
+        int secondSum = 0;
+
+        for (int i = 0; i < vector.size(); i++) {
+            firstSum += firstRow.get(i) * vector.get(i);
+            secondSum += secondRow.get(i) * vector.get(i);
+        }
+
+        TwoRowMatrix twoRowMatrix = new TwoRowMatrix();
+        twoRowMatrix.getFirstRow().add(firstSum);
+        twoRowMatrix.getSecondRow().add(secondSum);
+
+        return twoRowMatrix;
+    }
+
+    /**
+     * @return the firstRow
+     */
+    public ArrayList<Integer> getFirstRow() {
+        return firstRow;
+    }
+
+    /**
+     * @return the secondRow
+     */
+    public ArrayList<Integer> getSecondRow() {
+        return secondRow;
+    }
+
+    public ArrayList<Integer> getRow(int index) {
+        switch (index) {
+        case 0:
+            return getFirstRow();
+        case 1:
+            return getSecondRow();
+        default:
+            throw new ArrayIndexOutOfBoundsException(index);
+        }
+    }
+
 }
