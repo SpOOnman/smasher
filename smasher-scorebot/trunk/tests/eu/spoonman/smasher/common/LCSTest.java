caa291980725e96ca91416ea32733572c0ea3b8e
tomasz.kalkosinski <tomasz.kalkosinski@ebad3e58-8269-11dd-ad27-0f66e57d2035>
2009-02-20 00:13:38 +0000

    Add LCS missing tests.

--- smasher-scorebot/trunk/tests/eu/spoonman/smasher/common/LCSTest.java
+++ smasher-scorebot/trunk/tests/eu/spoonman/smasher/common/LCSTest.java
@@ -34,6 +34,13 @@ public class LCSTest {
 		return lcs.getLCSPairs();
 		
 	}
+	
+	private void printPairs(List<Pair<String,String>> pairs) {
+		for (Pair<String,String> pair : pairs) {
+			System.out.println(String.format("Left %s right %s", pair.getFirst(), pair.getSecond()));
+		}
+		
+	}
 
 	@Test
 	public void testGetLCSPairs() {
@@ -46,5 +53,38 @@ public class LCSTest {
 		
 		//Assert good order
 		assertEquals(pairs.get(0).getFirst(), "Ann");
+	}
+
+	@Test
+	public void testGetLCSPairsMissing() {
+		testGetLCSPairsMissingRight(0);
+		testGetLCSPairsMissingRight(3);
+		testGetLCSPairsMissingRight(5);
+		
+		testGetLCSPairsMissingLeft(0);
+		testGetLCSPairsMissingLeft(3);
+		testGetLCSPairsMissingLeft(5);
+	}
+	
+	public void testGetLCSPairsMissingRight(int index) {
+		List<String> right = getList();
+		right.remove(index);
+		
+		List<Pair<String,String>> pairs = getLCSPairs(getList(), right);
+		
+		assertEquals(right.size() + 1, pairs.size());
+		assertNotNull(pairs.get(index).getFirst());
+		assertNull(pairs.get(index).getSecond());
+	}
+	
+	public void testGetLCSPairsMissingLeft(int index) {
+		List<String> left = getList();
+		left.remove(index);
+		
+		List<Pair<String,String>> pairs = getLCSPairs(left, getList());
+		
+		assertEquals(left.size() + 1, pairs.size());
+		assertNull(pairs.get(index).getFirst());
+		assertNotNull(pairs.get(index).getSecond());
 	}
 }
