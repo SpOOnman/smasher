ac497f50a2737725830f299150cff31599270227
tomasz.kalkosinski <tomasz.kalkosinski@ebad3e58-8269-11dd-ad27-0f66e57d2035>
2009-04-24 14:56:29 +0000

    Scratch for this persister.

---
+++ smasher-serverinfo/trunk/src/eu/spoonman/smasher/serverinfo/persister/timeinfo/Quake3TimeInfoPersister.java
@@ -0,0 +1,39 @@
+/*
+ * This file is part of Smasher.
+ * Copyright 2008, 2009 Tomasz 'SpOOnman' Kalkosiński <spoonman@op.pl>
+ * 
+ * Smasher is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * Smasher is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Smasher.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package eu.spoonman.smasher.serverinfo.persister.timeinfo;
+
+import org.joda.time.Period;
+
+import eu.spoonman.smasher.serverinfo.ServerInfo;
+import eu.spoonman.smasher.serverinfo.persister.ServerInfoPersister;
+
+/**
+ * @author Tomasz Kalkosiński
+ *
+ */
+public class Quake3TimeInfoPersister implements ServerInfoPersister {
+    
+    private Period oldPeriod;
+    
+    @Override
+    public void persist(ServerInfo serverInfo) {
+        // TODO Auto-generated method stub
+    }
+
+}
