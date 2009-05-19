e841c5552048e97a5fd4a4e09ec9e7e9d1fb8a86
tomasz.kalkosinski <tomasz.kalkosinski@ebad3e58-8269-11dd-ad27-0f66e57d2035>
2009-05-19 09:21:11 +0000

    Write down some known hosts.

--- smasher-serverinfo/trunk/tests/eu/spoonman/smasher/serverinfo/CaptureResponseUtility.java
+++ smasher-serverinfo/trunk/tests/eu/spoonman/smasher/serverinfo/CaptureResponseUtility.java
@@ -29,9 +29,23 @@ import java.net.UnknownHostException;
  */
 public class CaptureResponseUtility {
     
+    private static String QL_IP_PL = "79.141.60.101";
+    private static String QL_IP_DE = "94.76.229.6";
+    private static String QL_IP_XX = "91.198.152.225";
+    
+    private static String QL_OLD1 = "92.48.121.75";
+    private static String QL_OLD2 = "85.25.86.234";
+    
     public static void main(String[] args) throws IOException {
-        InetAddress address = InetAddress.getByName("85.25.86.234"); //92.48.121.75 //85.25.86.234
-        ServerQuery serverQuery = ServerQueryManager.createServerQuery(Games.QUAKELIVE, address, 27025);
+        InetAddress address = InetAddress.getByName(QL_IP_PL); //92.48.121.75 //85.25.86.234
+        
+        for (int port = 27000 ; port < 27011 ; port++) {
+            query(address, port);
+        }
+    }
+
+    private static void query(InetAddress address, int port) throws UnknownHostException, IOException {
+        ServerQuery serverQuery = ServerQueryManager.createServerQuery(Games.QUAKELIVE, address, port);
         
         String captured = captureResponse(serverQuery);
         System.out.println(captured);
